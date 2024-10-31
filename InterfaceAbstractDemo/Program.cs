d987995df0f5d7ebd43b17f3218d1cc2b1f04800
Coşkun TEMENNİ <121826274+Ssminojansin@users.noreply.github.com>
2024-10-31 18:03:44 +0300

    Add files via upload

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+
+
+ class Program
+{
+     static void Main(string[] args)
+    {
+
+
+
+
+
+        BaseCustomerManager customerManager = new StarbuckCustomerManager(new MernisServiceAdapter());
+       customerManager.Save(new Customer {DateOfBirth = new DateTime(1994 , 08,10),FirstName="Coskun",LastName="Temenni",NationalityId="20428361118" });
+        
+    }
+
+    
+   
+}
